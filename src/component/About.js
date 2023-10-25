8698da0ed1c7c153881a0070a3d7427e2f6156f7
abeez07 <abrahamyusuf17@gmail.com>
2023-10-25 11:38:03 +0100

    update

--- src/component/About.js
+++ src/component/About.js
@@ -12,61 +12,66 @@ function About() {
    
     <Navbar />
       <AboutBanner />
-      <div style={{margin:'4.5rem 0.5rem'}} class="p-3 rounded-4 shadow row container mx-auto text-start align-items-center">
-            <div class="col-lg-6">
+      <div style={{margin:'4.5rem 0rem '}} class="p-3 col-10 row mx-auto text-start align-items-center">
+            <div class="col-lg-5">
               <h4 id='primary1' class="fs-6 my-3 text-dark">SHAPING A BRIGHT FUTURE.</h4>
-                <h1 id='primary' class="fs-3 my-4 mt-2 text-success">WE POSITION</h1>
-               <p id='parag' class="fs-5">People need what you're offering, but don't know why they should choose you over your competitors. With our branding and design services, we dress up your company  and ensure your customers can’t say NO to your offers.
-               </p>
-               <Link href="#" id='button' class="btn btn-primary">Go somewhere</Link>
+                <h1 id='primary' class="fs-3 my-4 mt-2 ">Wase Student Scholarship Scheme (WASSS)</h1>
+               <p id='parag' >Welcome to the "Wase Student Scholarship Scheme (WASSS)"! We are dedicated to empowering students and supporting their educational aspirations through our scholarship program. Our mission is to provide financial assistance and opportunities to deserving students, enabling them to achieve their academic goals and unlock their full potential.</p>
+
+<p>At the heart of our scholarship scheme is the belief that education is a powerful tool for personal and societal growth. We understand that many students face financial barriers that can hinder their educational pursuits, and we are committed to bridging that gap by offering scholarships to eligible individuals indigine of Wase Local Goverment.</p>
+<p>We are proud to be associated with the regime of His Excellency Governor Caleb Manasseh Mutfwang, a renowned Human Being committed to promoting excellence in education and humanity at large. With our scholarship program, we aim to foster a culture of meritocracy and empower students from diverse backgrounds to thrive academically.</p>
+
+
+               
+               <Link href="#" id='button' class="btn btn-primary">READ MORE</Link>
         
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-5 mx-auto">
           
   <img src={oga} class="card-img-top" alt="..."/>
-  {/* <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="card-body">
+    {/* <h5 class="card-title">Card title</h5> */}
+    <p class="card-text">We invite you to explore our website and learn more about the eligibility criteria, application process, and benefits of our scholarship program. Whether you are a prospective applicant, a dedicated supporter, or a potential partner, we welcome your involvement and collaboration. Together, we can empower talented individuals to shape a brighter future.</p>
    
-  </div> */}
+  </div>
 
             </div>
            
         </div>
-        <div style={{margin:'4.5rem 0.5rem'}} class="p-3 rounded-4 row justify-content-center text-center py-5 ">
-        <div class="col-md-12">
-            <h2 id='primary' class="mb-4 mx-auto shadow-sm col-md-6 pb-2" style={{borderBottom: '1px blue solid'}}>Our team <span class='text-success'>members</span><h5 CLASS='fs-6 mt-4'>SUSPENDISSE A ANTE IN  NEQUE IACULIS LACINIA</h5></h2>
+        <div  class=" rounded-4 row justify-content-center text-center ">
+        <div id='our-team' class="col-md-12 ">
+            <h2 id='primary' class="mb-4 mx-auto col-md-6 pb-2" >Our team <span id='member'>members</span><h5 CLASS='fs-6 mt-4'>SUSPENDISSE A ANTE IN  NEQUE IACULIS LACINIA</h5></h2>
             
             <div class="row justify-content-center">
-                <div id='review' class="col-lg-3 rounded shadow m-3 p-4">
+                <div class="col-lg-3  m-3 p-4 bg-white">
                     
-                    <div class="h4 text-black fs-3">Bill Gates</div>
+                    <div class="h4 text-black fs-3">Ado Ibrahim</div>
                     {/* <div class="text-muted mb-3 text-success">Founder, Microsoft</div> */}
                   
-                    <p class="mt-3 text-success">"JL Media transformed our processes, boosting productivity and saving costs."</p>
+                    <p id='review1'class="mt-3 ">"JL Media transformed our processes, boosting productivity and saving costs."</p>
                 </div>
-                <div id='review' class="col-lg-3 rounded shadow m-3 p-4">
+                <div  class="col-lg-3  m-3 p-4 bg-white">
                     {/* <img class="img-fluid rounded-circle mb-3" src={bill} alt="Client 1"/> */}
-                    <div class="h4 text-black fs-3">Bill Gates</div>
+                    <div class="h4 text-black fs-3">Kabiru abdullahi</div>
                     {/* <div class="text-muted mb-3">Founder, Microsoft</div> */}
              
-                    <p class="mt-3 text-success">"Our digital presence soared with this tech company's cutting-edge solutions."</p>
+                    <p id='review1' class="mt-3">"Our digital presence soared with this tech company's cutting-edge solutions."</p>
                 </div>
-                <div id='review' class="col-lg-3 rounded shadow m-3 p-4">
+                <div  class="col-lg-3  m-3 p-4 bg-white">
                     {/* <img class="img-fluid rounded-circle mb-3" src={bill} alt="Client 1"/> */}
-                    <div class="h4 text-black fs-3">Bill Gates</div>
+                    <div class="h4 text-black fs-3">Hajiya Inno</div>
                     {/* <div class="text-muted mb-3">Founder, Microsoft</div> */}
              
-                    <p class="mt-3 text-success">"Impressed by this tech company's seamless solutions. ."</p>
+                    <p id='review1' class="mt-3">"Impressed by this tech company's seamless solutions. ."</p>
                 </div>
             </div>
         </div>
         </div>
 
-                <div id="funfacts" style={{margin:'4.5rem 0.5rem'}} class="row text-start align-items-center">
+                <div id="funfacts" style={{margin:'0rem 0rem'}} class="row text-start align-items-center">
             <div id='aims' class="col-lg-6">
-                <h5 id='primary' class="fs-5 my-4 text-white">WASE STUDENT SCHOLARSHIP SCHEME</h5>
-                <h3 id='primary1' class="fs-5 my-4 text-white" >Aims and Objectives</h3>
+                <h5 id='primary' class="fs-6 my-4 text-white">WASE STUDENT SCHOLARSHIP SCHEME</h5>
+                <h3 id='primary1' class="fs-3 my-4 text-white" >Aims and Objectives</h3>
                 <h3 id='primary2' class="fs-5 my-4 text-success" >Summary</h3>
                <p id='parag' class="fs-5 text-white">The aim of the "Wase Student Scholarship Scheme" is to provide financial assistance and support to deserving students, empowering them to pursue higher education and achieve their academic and career aspirations
                </p>
--- src/component/Contact.js
+++ src/component/Contact.js
@@ -19,48 +19,53 @@ function Contact() {
                     <div class="h4 text-black">Phone</div>
                     <div class="text-muted mb-3">Call or send sms any time of the day</div>
                   
-                    <p class="mt-3 text-black">Along Emir Street Opp. Unity Bank PLC</p>
+                    <p id='info' class="mt-3 ">090-654-345-56</p>
                 </div>
                 <div id='review' class="col-lg-3 rounded shadow m-3">
                 <img className='m-3' width="50" height="50" src="https://img.icons8.com/fluency-systems-filled/48/02A12F/new-post.png" alt="new-post"/>
                     <div class="h4 text-black">Email</div>
                     <div class="text-muted mb-3">Fell free to send us messege</div>
              
-                    <p class="mt-3 text-black">info@wasss.com.ng</p>
+                    <p id='info'class="mt-3 ">info@wasss.com.ng</p>
                 </div>
                 <div id='review' class="col-lg-3 rounded shadow m-3">
                 <img className='m-3' width="50" height="50" src="https://img.icons8.com/ios-filled/50/02A12F/marker.png" alt="marker"/>
                     <div class="h4 text-black">Location</div>
                     <div class="text-muted mb-3">Along Emir Street Opp. Unity Bank PLC</div>
              
-                    <p class="mt-3 text-black">Along Emir Street Opp. Unity Bank PLC</p>
+                    <p id='info' class="mt-3 ">View on Goggle Maps</p>
                 </div>
             </div>
         </div>
   </div>
-<form  class="row g-3 col-8 mx-auto m-4">
+  <div id='our-team1'>
+  <form class="row g-3 col-8 mx-auto">
+<h1 class='text-center fs-2'>Send us a <span id='member'>message</span></h1>
+<p class='text-center fw-bold text-muted '>THIS IS ANOTHER OPTION YOU CAN USE TO CANTACT USS</p>
   <div class="col-md-4">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
+    <label for="inputText4" class="form-label"></label>
+    <input type="text" class="form-control" placeholder='Full Name' name="" id="" />
   </div>
   <div class="col-md-4">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
+    <label for="inputPassword4" class="form-label"></label>
+    <input type="email" class="form-control" placeholder='Email Address' name="" id="" />
   </div>
   <div class="col-md-4">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
+    <label for="inputPassword4" class="form-label"></label>
+    <input type="text" class="form-control" placeholder='Subject' name="" id="" />
+    
   </div>
   <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St "/>
+   <textarea class='form-control' placeholder='Your Message' id="floatingTextarea"></textarea>
   </div>
  
  
-  <div class="col text-center">
+  <div class="col text-center p-5">
     <button type="submit" class="btn btn-primary col-5">Sign in</button>
   </div>
 </form>
+  </div>
+
 <Footer/>
   </>
   )
